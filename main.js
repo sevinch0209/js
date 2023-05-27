5bd7f8d2a0fc5c0d1f0783192885a7aca2334bed
sevinch <sevinch>
2023-05-27 09:55:32 +0500

    asg

---
+++ main.js
@@ -0,0 +1,8 @@
+var user = prompt( "Enter your name");
+alert('Open the console to see the result')
+var userName = 'Sevinch'
+if (userName == user) {
+console.log('Hello boss');
+}else{
+    console.log('Hello guest');
+}
